3775578a2f12910f483f7ae9838977f27ef95d25
mozammal <mozammal@nascenia.com>
2017-06-23 10:15:55 +0600

    delete bogus

--- echatbot_1/src/main/java/com/nascenia/domain/repository/InventoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nascenia.domain.repository;
-
-import com.nascenia.domain.entity.Inventory;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-/** Created by mozammal on 6/6/17. */
-@Repository
-public interface InventoryRepository extends CrudRepository<Inventory, Long> {}
